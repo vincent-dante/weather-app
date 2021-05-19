4702099b16a71eb3446e9afd2381ca4e1c17c49e
Vincent Dante <vincent.dante96@gmail.com>
2021-05-20 03:06:02 +0800

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+
+      </div>
+    );
+  }
+}
+
+
+
+
+ReactDOM.render(<Game />, document.getElementById("root"));
